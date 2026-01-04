bb8057aba25388fac50bc707021ea9afff37cc42
Angus Chang <angus_chang@wistron.com>
2026-01-04 10:13:07 +0800

    Add ISR revalidation to homepage (1 hour)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import { getPosts } from '@/lib/notion';
 import PostList from '@/components/PostList';
 
+// Revalidate every 1 hour (same as post detail pages)
+export const revalidate = 3600;
+
 export default async function Home() {
   const posts = await getPosts();
 
